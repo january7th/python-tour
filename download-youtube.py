8b0a6c7419fd8edd2b19167f0cbdeeb54b1cdcab
duong.le <leminhduong179@gmail.com>
2025-02-15 11:10:58 +0700

    chore: add download youtube video function
    (cherry picked from commit a187ee88f11fc1b791c2fa82b9fdbdff560ecbb4)

---
+++ download-youtube.py
@@ -0,0 +1,18 @@
+import yt_dlp
+import os
+def download_youtube_video(url, output_path='.'):
+    os.makedirs(output_path, exist_ok=True)
+    ydl_opts = {
+        'format': 'bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best',
+        'outtmpl': f'{output_path}/%(title)s.%(ext)s',
+    }
+
+    try:
+        with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+            ydl.download([url])
+        print("Download completed successfully!")
+    except Exception as e:
+        print(f"An error occurred: {str(e)}")
+
+video_url = "https://www.youtube.com/watch?v=wGGjVP5MB4k&list=PLU0Ir_rHTlbASLn01AIDlotVD9E0Roj2P"
+download_youtube_video(video_url, output_path="data/khoa-hoc-alden-nguyen")
